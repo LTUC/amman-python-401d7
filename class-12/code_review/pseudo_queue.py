3eb6fff4baaf5e5641e5ec6792a656227f6d65ec
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2021-11-15 12:39:37 +0200

    Added demo and code review - class 12

---
+++ class-12/code_review/pseudo_queue.py
@@ -0,0 +1,18 @@
+from stack import Stack
+
+class PseudoQueue:
+    def __init__(self):
+        self.inn = Stack()
+        self.out = Stack()
+    
+    def enqueue(self, value):
+        # pop all vlaues from out and push them to innn
+        # push new value to inn
+        pass
+
+    def dequeue(self):
+        # check if inn has values, push them to out
+        # pop from out
+        pass
+
+
